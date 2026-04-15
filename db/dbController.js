b2257fc628e8e1a9b95f18215b61b131be5c251d
Anya Andrews <2019120080@ub.edu.bz>
2026-04-14 22:56:32 -0600

    Database Coonection

--- db/dbController.js
+++ db/dbController.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     host:     'localhost',
     port:     5432,
-    database: 'store_db',
+    database: 'receipt',
     user:     'postgres',
     password: 'password'
 });
